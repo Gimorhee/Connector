6482d71afec9c3d6690085bf6ee8b38744717eca
Danny Rhee <dongyunrhee@gmail.com>
2019-10-08 22:50:30 -0700

    Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -8,6 +8,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 
 const Profile = ({
   profile: { loading, profile },
@@ -68,6 +69,10 @@ const Profile = ({
                 <h4>No education crendentials</h4>
               )}
             </div>
+
+          {profile.githubusername && (
+            <ProfileGithub username={profile.githubusername} />
+          )}
           </div>
         </Fragment>
       )}
