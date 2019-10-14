9efff63a896a6690a8597a4a282fb740f27b469f
Danny Rhee <dongyunrhee@gmail.com>
2019-10-13 22:26:36 -0700

    Test

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
-const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 // Connect Database
@@ -17,14 +16,16 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
-
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+  
+  const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
